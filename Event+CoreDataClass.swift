8e1ed37a0e79190dd84ca2c53424c38bcf608c26
DONGSHUER <16251164@life.hkbu.edu.hk>
2020-12-06 20:27:42 +0800

    final commit

---
+++ Event+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Event+CoreDataClass.swift
+//  InfoDaySwiftUI
+//
+//  Created by DONG SHUER on 6/12/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Event: NSManagedObject {
+
+}
---
+++ InfoDaySwiftUI/Persistence.swift
@@ -0,0 +1,8 @@
+//
+//  Persistence.swift
+//  InfoDaySwiftUI
+//
+//  Created by DONG SHUER on 6/12/2020.
+//
+
+import Foundation
